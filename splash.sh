72922a58bcaa479e7616e46c1a428b4b1e412a78
Daniel Nordberg <dnordberg@gmail.com>
2014-11-24 12:10:36 +0300

    Docker run fix

--- splash.sh
+++ splash.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 maxrss=${SPLASH_MAXRSS:-$(awk '/MemTotal/{print $2*0.75/1024}' /proc/meminfo)}
 
-xvfb-run --server-args="+extension RANDR -screen 0 30000x7000x24" python -m splash.server \
+xvfb-run -e /dev/stdout --server-args="+extension RANDR -screen 0 30000x7000x24" python -m splash.server \
    --manhole \
    --maxrss $maxrss \
    --cache --cache-size=4096 \
-   --proxy-profiles-path=/opt/proxy_profiles \
-   --js-profiles-path=/opt/js_profiles
+   --proxy-profiles-path=/etc/splash/proxy-profiles \
+   --js-profiles-path=/etc/splash/js-profiles
 
